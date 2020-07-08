2437be1a55e694acb5161291582d89e26c595566
Mori01231 <mori01231mc@gmail.com>
2020-07-08 21:04:53 +0900

    [Add] Initial commit

---
+++ src/main/java/com/github/mori01231/eventpoints/EventPoints.java
@@ -0,0 +1,17 @@
+package com.github.mori01231.eventpoints;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventPoints extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
