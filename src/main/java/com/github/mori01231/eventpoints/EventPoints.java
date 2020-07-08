a1c80b7d03988affcc59d454978b2d49fc998a2f
Mori01231 <mori01231mc@gmail.com>
2020-07-08 21:30:37 +0900

    [Add] Connection with MySQL server

--- src/main/java/com/github/mori01231/eventpoints/EventPoints.java
+++ src/main/java/com/github/mori01231/eventpoints/EventPoints.java
@@ -3,6 +3,9 @@ package com.github.mori01231.eventpoints;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public final class EventPoints extends JavaPlugin {
 
@@ -15,16 +18,38 @@ public final class EventPoints extends JavaPlugin {
         // Plugin startup logic
         this.saveDefaultConfig();
 
-
         host = getConfig().getString("host");
         port = getConfig().getInt("port");
         database = getConfig().getString("database");
         username = getConfig().getString("username");
         password = getConfig().getString("Password");
+
+        try {
+            openConnection();
+            Statement statement = connection.createStatement();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void openConnection() throws SQLException, ClassNotFoundException {
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+
+        synchronized (this) {
+            if (connection != null && !connection.isClosed()) {
+                return;
+            }
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, this.username, this.password);
+        }
     }
 }
