ff9abb302510fa327dfeb9d7138022a3778e9153
Mori01231 <mori01231mc@gmail.com>
2020-07-08 21:23:49 +0900

    [Add] Config file

--- src/main/java/com/github/mori01231/eventpoints/EventPoints.java
+++ src/main/java/com/github/mori01231/eventpoints/EventPoints.java
@@ -2,12 +2,25 @@ package com.github.mori01231.eventpoints;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+
 public final class EventPoints extends JavaPlugin {
+
+    private Connection connection;
+    private String host, database, username, password;
+    private int port;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.saveDefaultConfig();
 
+
+        host = getConfig().getString("host");
+        port = getConfig().getInt("port");
+        database = getConfig().getString("database");
+        username = getConfig().getString("username");
+        password = getConfig().getString("Password");
     }
 
     @Override
