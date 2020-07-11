25b6e4151e77fad4d50da4a577196b5ba772402a
Mori01231 <mori01231mc@gmail.com>
2020-07-12 03:35:13 +0900

    [Update] Convert command can now connect to database

--- src/main/java/com/github/mori01231/eventpoints/CommandExecutors/ConvertCommandExecutor.java
+++ src/main/java/com/github/mori01231/eventpoints/CommandExecutors/ConvertCommandExecutor.java
@@ -1,12 +1,124 @@
 package com.github.mori01231.eventpoints.CommandExecutors;
 
+import com.github.mori01231.eventpoints.EventPoints;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.sql.*;
+
+import static org.bukkit.Bukkit.getLogger;
 
 public class ConvertCommandExecutor implements CommandExecutor {
+
+    public Connection connection;
+    private String host, database, username, password;
+    private int port;
+
+    private String DatabaseName;
+    private String ConvertMode;
+    private Boolean isConsole;
+    Player player;
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        //Check if person executing command is player
+        if ((sender instanceof Player)) {
+            player = (Player) sender;
+        } else{
+            getLogger().info(ChatColor.translateAlternateColorCodes('&',"このコマンドはコンソールからは実行できません。"));
+            return true;
+        }
+
+        //Not enough arguments
+        if(args.length == 0){
+            FeedBack("&c作成するイベントポイントの名前を指定してください 例：/epc lgw_summer_2020 item");
+            return true;
+        }
+        if(args.length == 1){
+            FeedBack("&cポイントに変換するかアイテムに変換するか指定してください 例：/epc lgw_summer_2020 points");
+            return true;
+        }
+
+        String PlayerName = player.getName();
+        String PlayerUUID = String.valueOf(player.getUniqueId());
+
+        DatabaseName = args[0];
+
+        //Convert any valid ConvertModes into one ConvertMode
+        ConvertMode = args[1];
+        if (ConvertMode == "item" || ConvertMode == "items" || ConvertMode == "i"){
+            ConvertMode = "item";
+        }
+        if (ConvertMode == "points" || ConvertMode == "point" || ConvertMode == "p"){
+            ConvertMode = "points";
+        }
+
+        //Invalid ConvertMode
+        if (!(ConvertMode == "item" || ConvertMode == "points")){
+            FeedBack("&cイベントポイントをアイテムに変換する場合は変換方式にitemを指定してください 例：/epc lgw_summer_2020 item");
+            FeedBack("&cアイテムをイベントポイントに変換する場合は変換方式にpointsを指定してください 例：/epc lgw_summer_2020 points");
+            return true;
+        }
+
+        BukkitRunnable r = new BukkitRunnable() {
+            @Override
+            public void run() {
+                //This is where you should do your database interaction
+
+                try {
+                    openConnection();
+                    Statement statement = connection.createStatement();
+
+                    ResultSet result = statement.executeQuery("SHOW TABLES LIKE '" + DatabaseName + "';");
+                    if (result.next() == false) {
+                        FeedBack("&c" + DatabaseName + "という名前のイベントポイントは存在しません。/epn " + DatabaseName + " コマンドで先にそのイベントポイントを作成してください。");
+                    } else {
+                        if(ConvertMode == "item"){
+
+                        }
+                        if(ConvertMode == "points"){
+
+                        }
+                    }
+
+                } catch(ClassNotFoundException | SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        r.runTaskAsynchronously(EventPoints.getInstance());
+
+        return true;
+    }
+
+    public void openConnection() throws SQLException, ClassNotFoundException {
+
+        host = EventPoints.getInstance().getConfig().getString("host");
+        port = EventPoints.getInstance().getConfig().getInt("port");
+        database = EventPoints.getInstance().getConfig().getString("database");
+        username = EventPoints.getInstance().getConfig().getString("username");
+        password = EventPoints.getInstance().getConfig().getString("password");
+
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+
+        synchronized (this) {
+            if (connection != null && !connection.isClosed()) {
+                return;
+            }
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, this.username, this.password);
+        }
+    }
+
+    public void FeedBack(String message){
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&',message));
     }
 }
