04c5eb585bdfc6aefd70cf1de74ba46b33658010
Mori01231 <mori01231mc@gmail.com>
2020-07-18 01:35:28 +0900

    [Update] Sorted code to make ConvertEventPoints method do actual converting

--- src/main/java/com/github/mori01231/eventpoints/CommandExecutors/ConvertCommandExecutor.java
+++ src/main/java/com/github/mori01231/eventpoints/CommandExecutors/ConvertCommandExecutor.java
@@ -24,13 +24,16 @@ public class ConvertCommandExecutor implements CommandExecutor {
     private int port;
 
     private String DatabaseName;
-    private String ConvertMode;
+    private String DisplayName;
+    private int ConvertMode;
     private int currentPoints;
     private Boolean AmountIsIndicated = true;
     Player player;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
 
         //Check if person executing command is player
         if ((sender instanceof Player)) {
@@ -58,20 +61,39 @@ public class ConvertCommandExecutor implements CommandExecutor {
 
         DatabaseName = args[0];
 
+        //get display name of event point ticket
+        DisplayName = "§b" + DatabaseName;
+
         //Convert any valid ConvertModes into one ConvertMode
-        ConvertMode = args[1];
-        if (ConvertMode == "item" || ConvertMode == "items" || ConvertMode == "i"){
-            ConvertMode = "item";
+        ConvertMode = 0;
+        if (args[1] == "item" || args[1] == "items" || args[1] == "i"){
+            ConvertMode = 1;
         }
-        if (ConvertMode == "points" || ConvertMode == "point" || ConvertMode == "p"){
-            ConvertMode = "points";
+        if (args[1] == "points" || args[1] == "point" || args[1] == "p"){
+            ConvertMode = 2;
         }
 
         //Invalid ConvertMode
-        if (!(ConvertMode == "item" || ConvertMode == "points")){
+        if (ConvertMode == 0){
             FeedBack("&cイベントポイントをアイテムに変換する場合は変換方式にitemを指定してください 例：/epc lgw_summer_2020 item");
             FeedBack("&cアイテムをイベントポイントに変換する場合は変換方式にpointsを指定してください 例：/epc lgw_summer_2020 points");
             return true;
+        }
+
+        int convertAmount;
+        if (AmountIsIndicated){
+            try {
+                convertAmount = Integer.parseInt(args[2]);
+                if (convertAmount < 0){
+                    FeedBack("&c&l変換するアイテム数は正の整数で指定してください");
+                    return true;
+                }
+            }catch(Exception e){
+                FeedBack("&c&l変換するアイテム数は正の整数で指定してください");
+                return true;
+            }
+        }else{
+            convertAmount = -1;
         }
 
         BukkitRunnable r = new BukkitRunnable() {
@@ -93,6 +115,7 @@ public class ConvertCommandExecutor implements CommandExecutor {
                         } else {
                             Integer currentPoints = Integer.valueOf(findPlayer.getString("points"));
                         }
+                        ConvertEventPoints(player, currentPoints, ConvertMode, convertAmount);
                     }
 
                 } catch(ClassNotFoundException | SQLException e) {
@@ -106,6 +129,51 @@ public class ConvertCommandExecutor implements CommandExecutor {
         FeedBack("&aYou have " + currentPoints + " points.");
 
         return true;
+    }
+
+
+
+    public void ConvertEventPoints(Player player, int hasPoints, int mode, int convertPoints){
+
+        // item mode
+        if (mode == 1){
+
+            // get player inventory.
+            Inventory inv = player.getInventory();
+
+            // initializing counter for tickets and points.
+            int tickets = 0;
+            int points = 0;
+
+            //counting and deleting tickets.
+            for (ItemStack item: inv.getContents()) {
+                try{
+                    if(item.getItemMeta().getDisplayName().equals(DisplayName)){
+                        tickets += item.getAmount();
+                        item.setAmount(0);
+                    }
+
+                }catch (Exception e){
+                }
+            }
+
+            points = tickets;
+
+            //Give player ontime points.
+            getServer().dispatchCommand(getServer().getConsoleSender(), "epa " + DatabaseName + " " + player.getName() + " " + points);
+
+            //Create the message to be sent to the player
+            String ReturnMessage = "&b" + DatabaseName + "チケット" + tickets + "枚を" + DatabaseName + "ポイント" + points + "ポイントに変換しました。";
+
+            // Send message to player
+            FeedBack(ReturnMessage);
+        }
+
+        // points mode
+        if (mode == 2){
+
+        }
+
     }
 
 
@@ -159,42 +227,6 @@ public class ConvertCommandExecutor implements CommandExecutor {
 
         //return the number of available slots.
         return slots;
-    }
-
-    public String ConvertEventPointTickets(Player player){
-
-        //get display name of event point ticket
-        String DisplayName = DatabaseName;
-
-        //get player inventory.
-        Inventory inv = player.getInventory();
-
-        //initializing counter for tickets and points.
-        int tickets = 0;
-        int points = 0;
-
-        //counting and deleting tickets.
-        for (ItemStack item: inv.getContents()) {
-            try{
-                if(item.getItemMeta().getDisplayName().equals(DisplayName)){
-                    tickets += item.getAmount();
-                    item.setAmount(0);
-                }
-
-            }catch (Exception e){
-            }
-        }
-
-        points = tickets;
-
-        //Give player ontime points.
-        getServer().dispatchCommand(getServer().getConsoleSender(), "epa " + DatabaseName + " " + player.getName() + " " + points);
-
-        //Create the message to be sent to the player
-        String ReturnMessage = "&b" + DatabaseName + "チケット" + tickets + "枚を" + DatabaseName + "ポイント" + points + "ポイントに変換しました。";
-
-        //Return the message to be sent to the player
-        return ReturnMessage;
     }
 
     public void FeedBack(String message){
